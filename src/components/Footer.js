d9be0df74b5a55b7e5bbcf76794ee027f559b66e
Jeremy Eissler <jeissler0@gmail.com>
2018-08-22 12:05:08 -0500

    basic styles refactored hero, header, footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const year = new Date().getFullYear();
 
 const Footer = () => {
     return (
-        <footer className="app-footer">
+        <footer className="footer">
             <small>&copy; good life. {year}</small>
         </footer>
     )
