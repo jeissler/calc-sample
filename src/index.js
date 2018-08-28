a69391f9ce373479dccf1715327ad096ba1d3c87
Jeremy Eissler <jeissler0@gmail.com>
2018-08-28 18:17:04 -0500

    basic postcss plugin use to test preset-env config

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/index.css';
 import App from './App';
+
+import "./css/vars.css";
+import './css/index.css';
+
 //import registerServiceWorker from './registerServiceWorker';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 //registerServiceWorker();
