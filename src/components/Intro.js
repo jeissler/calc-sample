b650c2b09833a5dfb9b5ff4af7cb9fd80c592759
Jeremy Eissler <jeissler0@gmail.com>
2018-08-30 12:39:52 -0500

    button styles and markup fixes

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -8,10 +8,8 @@ const Intro = () => {
                 Get an honest look at your financial future and find out how to make the most of your assets.
             </p>
 
-            <Link to="/calc">
-                <button>
-                    Show Me How
-                </button>
+            <Link to="/calc" className="intro__btn">
+                Find Out How
             </Link>
         </section>
     )
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,17 +1,17 @@
 import React, {Fragment} from "react";
 
 const Error = () => {
-    return "Oh no, we've reached an error state!"
+    return "Oh no, we're unable to calculate your savings and retirement!"
 };
 
 const Result = (props) => {
     return (
         <Fragment>
-            {props.retire_yrs} years to retirement
-            <br/>
-            {props.life_total} total needed
-            <br/>
-            {props.life_savings} total saved
+            <h3>{props.retire_yrs} years to retirement</h3>
+            <h3>{props.life_total} total needed&sup1;</h3>
+            <h3>{props.life_savings} total saved</h3>
+
+            {/* TODO: add charts, animation, etc. */}
         </Fragment>
     )
 };
